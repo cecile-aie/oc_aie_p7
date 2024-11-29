9a0ba0c90b691d199a2b2834de71071be07c3de9
Cécile MAYER <cecilemayer@live.com>
2024-11-29 21:29:57 +0100

    Correction variable REQUIRED_FILES manquante

--- test/test_model_availability.py
+++ test/test_model_availability.py
@@ -2,8 +2,10 @@ import os
 import pytest
 from azure.storage.blob import BlobServiceClient
 
-AZURE_CONNECTION_STRING = os.environ.get("AZURE_STORAGE_CONNECTION_STRING")
-AZURE_CONTAINER_NAME = "oc-p7-ecomodele"
+# Paramètres Azure
+AZURE_CONNECTION_STRING = os.environ.get('AZURE_STORAGE_CONNECTION_STRING')  
+AZURE_CONTAINER_NAME = "oc-p7-ecomodele"  # Nom du conteneur Azure
+REQUIRED_FILES = ["MLmodel", "conda.yaml", "model.pkl"]  # Fichiers nécessaires
 
 @pytest.fixture
 def azure_blob_client():
@@ -12,14 +14,13 @@ def azure_blob_client():
     """
     if not AZURE_CONNECTION_STRING:
         pytest.fail("AZURE_STORAGE_CONNECTION_STRING n'est pas défini.")
-    
     try:
         blob_service_client = BlobServiceClient.from_connection_string(AZURE_CONNECTION_STRING)
         container_client = blob_service_client.get_container_client(AZURE_CONTAINER_NAME)
         return container_client
     except Exception as e:
         pytest.fail(f"Erreur de connexion au Blob Storage : {str(e)}")
-        
+
 def test_files_in_blob_container(azure_blob_client):
     """
     Teste si les fichiers requis pour le modèle sont présents dans le conteneur Azure Blob Storage.
